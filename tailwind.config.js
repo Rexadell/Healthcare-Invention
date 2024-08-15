5afa085223a3f5011996bb40c41de561f4c269a9
Rexadell <reksaparamaaditya@gmail.com>
2024-08-15 22:25:02 +0800

    navbar 1.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['/src/**/*.{html, js}'],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#50C878'
+      },
+      fontFamily: {
+        'inter': 'Inter'
+      },
+      keyframes: {
+      }
+    },
   },
   plugins: [],
 }
